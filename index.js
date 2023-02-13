c5b5b4798b12dddc28e0a0b273796ce352ec0481
Asif Al Mahmud <asifalmahmud1990@gmail.com>
2023-02-13 12:16:06 +0600

    create a txt file with some texts

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ const fs = require("fs");
 
 const name = fs.readFileSync("./texts/name.txt", "utf-8");
 console.log({ name });
+
+const textOut = `Thic is what you know about me: ${name}.`;
+fs.writeFileSync("./texts/what-you-know.txt", textOut);
