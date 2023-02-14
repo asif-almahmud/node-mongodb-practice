908145058a773f7fd9e4acbadc76fc3ef8b8b7ac
Asif Al Mahmud <asifalmahmud1990@gmail.com>
2023-02-14 10:25:36 +0600

    change root folder expression and send json data from the server

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const http = require("http");
+const fs = require("fs");
 const url = require("url");
 
 const server = http.createServer((req, res) => {
   const pathname = req.url;
-  if (pathname === "/home" || pathname === "/home") {
-    res.end("This is the home page");
-  } else if (pathname === "/product") {
+  if (pathname === "/api" || pathname === "/api/home") {
+    fs.readFile(`${__dirname}/data/data.json`, "utf-8", (err, data) => {
+      const productData = JSON.parse(data);
+      console.log({ productData });
+      res.writeHead(200, {
+        "Content-type": "application/json",
+      });
+      res.end(data);
+    });
+  } else if (pathname === "api/product") {
     res.end("This is the product page");
   } else {
     res.writeHead(404, {
